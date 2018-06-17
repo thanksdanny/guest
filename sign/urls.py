da6edaeb4017299a86f80151ff989519e50b1f90
thanksdanny <hj-07@163.com>
2018-06-17 21:22:27 +0800

    添加api

---
+++ sign/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from sign import views_if
+
+urlpatterns = [
+    # sign system interface:
+    # ex : /api/add_event/
+    url(r'^add_event/', views_if.add_event, name='add_event'),
+    # ex : /api/get_event_list/
+    url(r'^add_guess/', views_if.add_guest, name='add_guess'),
+    # ex : /api/get_event_list
+    url(r'^get_event_list/', views_if.get_event_list, name='get_event_list'),
+    # ex : /api/get_guess_list
+    url(r'^get_guess_list/', views_if.get_guest_list, name='get_guess_list'),
+    # ex : /api/user_sign/
+    url(r'^user_sign/', views_if.user_sign, name='user_sign'),
+
+]
+
+

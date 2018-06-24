8430d5b31fdf1df01f034eb3cb17190aaa0ff83a
thanksdanny <hj-07@163.com>
2018-06-24 16:57:55 +0800

    添加发布会接口---增加签名+时间戳

--- sign/view_if_sec.py
+++ sign/view_if_sec.py
@@ -1,6 +1,7 @@
 from django.contrib import auth as django_auth
 from django.http import JsonResponse
 import base64
+import time, hashlib
 
 # 用户认证
 
@@ -30,4 +31,48 @@ def get_event_list(request):
     eid = request.GET.get('eid', "") # 发布会id
     name = request.GET.get('name', "") # 发布会名称
 
+# 用户签名+时间戳
+def user_sign(request):
+    if request.method == 'POST':
+        client_time = request.POST.get('time', '') # 客户端时间戳
+        client_sign = request.POST.get('sign', '') # 客户端签名
+
+    else:
+        return "error"
+
+    if client_time == '' or client_sign == '':
+        return "sign null"
+
+    # 服务器时间
+    now_time = time.time()
+    server_time = str(now_time).split('.'[0])
+    # 获取时间差
+    time_difference = int(server_time) - int(client_time)
+    if time_difference >= 60:
+        return "timeout"
+
+    # 签名检查
+    md5 = hashlib.md5()
+    sign_str = client_time + "&Guest-Bugmaster"
+    sign_byte_utf8 = sign_str.encode(encoding="utf-8")
+    md5.update(sign_byte_utf8)
+    server_sign = md5.hexdigest()
+
+    if server_sign != client_sign:
+       return "sign fail"
+    else:
+        return "sign success"
+
+
+# 添加发布会接口---增加签名+时间戳
+def add_event(request):
+    sign_result = user_sign(request)
+    if sign_result == "error":
+        return JsonResponse({'status': 10011, 'message': 'request error'})
+    elif sign_result == "sign null":
+        return JsonResponse({'status': 10012, 'message': 'user sign null'})
+    elif sign_result == "timeout":
+        return JsonResponse({'status': 10013, 'message': 'user sign timeout'})
+    elif sign_result == "sign  fail":
+        return JsonResponse({'status': 10014, 'message': 'user sign error'})
 
